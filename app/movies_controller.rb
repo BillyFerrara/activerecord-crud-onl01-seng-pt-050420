249acc35dfbef61ad35820376c5d3066a549745a
Billy Ferrara <willfira717@gmail.com>
2020-07-21 21:10:19 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -63,7 +63,7 @@ end
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by
   # release date descending
-  movie = Movie.where(release_date: > 2002).order(release_date: :desc)
+  movie = Movie.where("release_date > 2002").order(release_date: :desc)
   __
 end
 
@@ -100,5 +100,5 @@ def can_destroy_all_items_at_once
   10.times do |i|
     Movie.create(title: "Movie_#{i}")
   end
-  Movie.destroy_all 
+  Movie.destroy_all
 end
